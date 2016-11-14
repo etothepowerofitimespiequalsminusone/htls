3c6cc7a1428d67166bbadddabcea338dcd4bd587
Darja Solodovnikova <darja.solodovnikova@lu.lv>
2016-11-14 21:35:10 +0200

    fillable fields of hotel added

--- app/Hotel.php
+++ app/Hotel.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Hotel extends Model
 {
+    protected $fillable = [
+        'name', 'description', 'stars', 'price_single', 'price_double', 'price_triple'
+    ];
     public function city() { 
         return $this->belongsTo('App\City');
     }
